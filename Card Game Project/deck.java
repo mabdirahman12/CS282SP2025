189e4acb29b58ca6735391ba94f42697ae8a35f8
mabdirahman12 <muhsin.abdirahman1@gmail.com>
2025-05-29 11:06:43 -0700

    Create deck.java

---
+++ Card Game Project/deck.java
@@ -0,0 +1,35 @@
+
+import java.util.*;
+
+public final class Deck {
+    private final List<Card> cards;
+
+    public Deck() {
+        cards = new ArrayList<>();
+        String[] suits = {"Hearts", "Diamonds", "Clubs", "Spades"};
+        String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
+
+        for (String suit : suits) {
+            for (int i = 0; i < ranks.length; i++) {
+                cards.add(new Card(suit, ranks[i], i + 2));
+            }
+        }
+
+        shuffle();
+    }
+
+    public void shuffle() {
+        Collections.shuffle(cards);
+    }
+
+    public Card dealCard() {
+        if (!cards.isEmpty()) {
+            return cards.remove(0);
+        }
+        return null;
+    }
+
+    public boolean isEmpty() {
+        return cards.isEmpty();
+    }
+}
